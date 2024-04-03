4d0e15c37b0111e3665ce45f80c38ba389727a01
liyubrt <li.yu@bluerivertech.com>
2024-04-03 16:57:15 -0700

    update query with databricks

--- save_image.py
+++ save_image.py
@@ -5,12 +5,14 @@ import shutil
 import random
 import pickle
 import imageio
+import logging
 import numpy as np
 import pandas as pd
+from PIL import Image
 from collections import defaultdict
 from tqdm import tqdm
 from pprint import pprint
-import matplotlib.pyplot as plt
+
 
 DEFAULT_TONEMAP_PARAMS = {"policy": "tonemap", "alpha": 0.25, "beta": 0.9, "gamma": 0.9, "eps": 1e-6}
 def normalize_image(image, hdr_mode=True, normalization_params=DEFAULT_TONEMAP_PARAMS, return_8_bit=False):
@@ -111,49 +113,76 @@ def normalize_image(image, hdr_mode=True, normalization_params=DEFAULT_TONEMAP_P
     return image
 
 
-data_root_dir = '/data/jupiter/li.yu/data'
-unlabeled_datasets = ["Jupiter_2023_03_29_10pm_30_3pm_Loamy_812_stops_stereo_2", 
-                      "Jupiter_2023_04_05_loamy869_dust_collection_stereo", 
-                      "Jupiter_2023_may_loamy731_vehicle_dust_human_stereo"]
-labeled_datasets = ["Jupiter_2023_03_02_and_2930_human_vehicle_in_dust_labeled", 
-                    "Jupiter_2023_March_29th30th_human_vehicle_in_dust_front_pod_labeled", 
-                    "Jupiter_2023_04_05_loamy869_dust_collection_stereo_labeled", 
-                    "Jupiter_2023_may_loamy731_vehicle_dust_human_stereo_labeled"]
-pred_root = '/data/jupiter/li.yu/exps/driveable_terrain_model/'
+# data_root_dir = '/data/jupiter/li.yu/data'
+# unlabeled_datasets = ["Jupiter_2023_03_29_10pm_30_3pm_Loamy_812_stops_stereo_2", 
+#                       "Jupiter_2023_04_05_loamy869_dust_collection_stereo", 
+#                       "Jupiter_2023_may_loamy731_vehicle_dust_human_stereo"]
+# labeled_datasets = ["Jupiter_2023_03_02_and_2930_human_vehicle_in_dust_labeled", 
+#                     "Jupiter_2023_March_29th30th_human_vehicle_in_dust_front_pod_labeled", 
+#                     "Jupiter_2023_04_05_loamy869_dust_collection_stereo_labeled", 
+#                     "Jupiter_2023_may_loamy731_vehicle_dust_human_stereo_labeled"]
+# pred_root = '/data/jupiter/li.yu/exps/driveable_terrain_model/'
 
-i = 0
+# i = 0
 
-# os.makedirs(os.path.join(data_root_dir, unlabeled_datasets[i], 'normed_images'), exist_ok=True)
-# master_df = pd.read_csv(os.path.join(data_root_dir, unlabeled_datasets[i], 'master_annotations.csv'), low_memory=False)
+# # os.makedirs(os.path.join(data_root_dir, unlabeled_datasets[i], 'normed_images'), exist_ok=True)
+# # master_df = pd.read_csv(os.path.join(data_root_dir, unlabeled_datasets[i], 'master_annotations.csv'), low_memory=False)
+# # print(master_df.shape)
+# # for idx, row in master_df.iterrows():
+# #     data_path = os.path.join(data_root_dir, unlabeled_datasets[i], 'processed/images', row.id, 'stereo_output.npz')
+# #     img = np.load(data_path)['left']
+# #     img_norm = normalize_image(img, row.hdr_mode)
+# #     img_norm = (img_norm * 255).astype(np.uint8)
+# #     img_norm = cv2.cvtColor(img_norm, cv2.COLOR_RGB2BGR)
+# #     save_path = os.path.join(data_root_dir, unlabeled_datasets[i], 'normed_images', row.id+'.png')
+# #     cv2.imwrite(save_path, img_norm)
+# #     if idx % 1000 == 0:
+# #         print(f'saved {idx+1} images')
+
+# os.makedirs(os.path.join(data_root_dir, unlabeled_datasets[i], 'normed_labels'), exist_ok=True)
+# master_df = pd.read_csv(os.path.join(data_root_dir, labeled_datasets[i], 'master_annotations.csv'), low_memory=False)
 # print(master_df.shape)
 # for idx, row in master_df.iterrows():
-#     data_path = os.path.join(data_root_dir, unlabeled_datasets[i], 'processed/images', row.id, 'stereo_output.npz')
-#     img = np.load(data_path)['left']
-#     img_norm = normalize_image(img, row.hdr_mode)
-#     img_norm = (img_norm * 255).astype(np.uint8)
-#     img_norm = cv2.cvtColor(img_norm, cv2.COLOR_RGB2BGR)
-#     save_path = os.path.join(data_root_dir, unlabeled_datasets[i], 'normed_images', row.id+'.png')
-#     cv2.imwrite(save_path, img_norm)
+#     label_path = os.path.join(data_root_dir, labeled_datasets[i], row.rectified_label_save_path)
+#     lbl = np.load(label_path)['left']
+#     save_path = os.path.join(data_root_dir, unlabeled_datasets[i], 'normed_labels', row.id+'.png')
+#     cv2.imwrite(save_path, lbl)
 #     if idx % 1000 == 0:
 #         print(f'saved {idx+1} images')
 
-os.makedirs(os.path.join(data_root_dir, unlabeled_datasets[i], 'normed_labels'), exist_ok=True)
-master_df = pd.read_csv(os.path.join(data_root_dir, labeled_datasets[i], 'master_annotations.csv'), low_memory=False)
-print(master_df.shape)
-for idx, row in master_df.iterrows():
-    label_path = os.path.join(data_root_dir, labeled_datasets[i], row.rectified_label_save_path)
-    lbl = np.load(label_path)['left']
-    save_path = os.path.join(data_root_dir, unlabeled_datasets[i], 'normed_labels', row.id+'.png')
-    cv2.imwrite(save_path, lbl)
-    if idx % 1000 == 0:
-        print(f'saved {idx+1} images')
+# master_df = pd.read_csv(os.path.join(data_root_dir, labeled_datasets[i+1], 'master_annotations.csv'), low_memory=False)
+# print(master_df.shape)
+# for idx, row in master_df.iterrows():
+#     label_path = os.path.join(data_root_dir, labeled_datasets[i+1], row.rectified_label_save_path)
+#     lbl = np.load(label_path)['left']
+#     save_path = os.path.join(data_root_dir, unlabeled_datasets[i], 'normed_labels', row.id+'.png')
+#     cv2.imwrite(save_path, lbl)
+#     if idx % 1000 == 0:
+#         print(f'saved {idx+1} images')
 
-master_df = pd.read_csv(os.path.join(data_root_dir, labeled_datasets[i+1], 'master_annotations.csv'), low_memory=False)
-print(master_df.shape)
-for idx, row in master_df.iterrows():
-    label_path = os.path.join(data_root_dir, labeled_datasets[i+1], row.rectified_label_save_path)
-    lbl = np.load(label_path)['left']
-    save_path = os.path.join(data_root_dir, unlabeled_datasets[i], 'normed_labels', row.id+'.png')
-    cv2.imwrite(save_path, lbl)
-    if idx % 1000 == 0:
-        print(f'saved {idx+1} images')
+
+
+# root_dir = '/data/jupiter/li.yu/data'
+# root_dir = '/data/jupiter/datasets/'
+root_dir = '/data2/jupiter/datasets/'
+dataset = 'rev1_train_v6_2'
+# dataset = 'halo_rgb_stereo_train_v8_0'
+csv = os.path.join(root_dir, dataset, 'master_annotations.csv')
+# converters = {"label_map": ast.literal_eval, "label_counts": ast.literal_eval}
+converters = {}
+df = pd.read_csv(csv, converters=converters)
+logging.info(f'{df.shape}')
+
+save_dir = '/data/jupiter/datasets/20240301_5_million_for_self_supervised_part_0'
+rectified_dir = os.path.join(save_dir, 'rev1_train')
+os.makedirs(rectified_dir, exist_ok=True)
+for i,row in df.iterrows():
+    data_path = os.path.join(root_dir, dataset, row.stereo_pipeline_npz_save_path)
+    img = np.load(data_path)['left']
+    img_norm = normalize_image(img, hdr_mode=row.hdr_mode, return_8_bit=True)
+    Image.fromarray(img_norm).save(os.path.join(rectified_dir, row.unique_id+'.jpg'))
+    if (i+1) % 2000 == 0:
+        logging.info(f'processed {i+1} images')
+saved_ids = df.unique_id.to_list()
+saved_df = pd.DataFrame(data={'id': [f'rev1_train/{f}.jpg' for f in saved_ids]})
+saved_df.to_csv(os.path.join(save_dir, 'rev1_train_saved_ids.csv'), index=False)
